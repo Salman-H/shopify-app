13aa9ff609f0848e9facc294ea6889c8154a4d2a
Salman <salman.hashmi2@mail.mcgill.ca>
2016-09-30 18:17:30 +0000

    correct typos in product hash methods

--- shop_module.rb
+++ shop_module.rb
@@ -5,14 +5,14 @@ module Shop
     $products_price_hash = Hash.new
     
     
-    # create a hash of product, price pairs from products_to_find
-    def Shop.create_hashes(products_to_find)
+    # create a hash of |product => count| pairs from products_to_find
+    def Shop.create_count_hash(products_to_find)
         $products_count_hash = Hash[products_to_find.collect { |product| [product, 0] }]
     end
     
     
-    # create a hash of product, product count pairs from products_to_find
-    def Shop.create_hashes(products_to_find)
+    # create a hash of |product => price| pairs from products_to_find
+    def Shop.create_price_hash(products_to_find)
         $products_price_hash = Hash[products_to_find.collect { |product| [product, 0.0] }]
     end
     
