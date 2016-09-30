48b98d0805da8d084922e287921f91489e3116d5
Salman <salman.hashmi2@mail.mcgill.ca>
2016-09-30 15:16:35 +0000

    print some common http status codes

--- url_module.rb
+++ url_module.rb
@@ -13,4 +13,25 @@ module Url
         end
     end
     
+    # print some common http status codes
+    def Url.print_status(code)
+        case code
+            when 200
+                puts "200 OK"
+            when 301
+                puts "301 Moved Permanently"
+            when 400
+                puts "400 Bad Request"
+            when 401
+                puts "401 Unauthorized"
+            when 403
+                puts "403 Forbidden"
+            when 404
+                puts "404 Not Found"
+            when 500...600
+                #puts "ZOMG ERROR #{response.code}"
+                puts "500..600"
+        end
+    end
+    
 end
