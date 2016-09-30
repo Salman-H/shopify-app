1e8ad9aa0da8e984c7a8e1eadd27e85779d8c354
Salman <salman.hashmi2@mail.mcgill.ca>
2016-09-30 18:48:47 +0000

    call prompt methods to handle inputs

--- app.rb
+++ app.rb
@@ -3,3 +3,8 @@ $LOAD_PATH << '.'
 require 'shop_module.rb'
 require 'url_module.rb'
 require 'input_module.rb'
+
+
+# call prompt methods to handle user inputs
+url = Input.prompt_shop_url
+products_to_find = Input.prompt_products_list
