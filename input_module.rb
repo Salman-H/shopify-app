37e8c4f3d37b5568d2b61ffdae42157364d4a352
Salman <salman.hashmi2@mail.mcgill.ca>
2016-09-30 15:09:54 +0000

    add method to check if a string is all digits

--- input_module.rb
+++ input_module.rb
@@ -5,4 +5,9 @@ module Input
     def Input.all_letters(str)
         str[/[a-zA-Z\s]+/]  == str
     end
+    
+    # check if a string is all digits
+    def Input.all_digits(str)
+        str[/[0-9]+/]  == str
+    end
 end
