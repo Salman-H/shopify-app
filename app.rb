6d72f9febca46c257d5a8f2676e2454cade3ae19
Salman <salman.hashmi2@mail.mcgill.ca>
2016-09-30 18:56:24 +0000

    process product results for each page until end of json

--- app.rb
+++ app.rb
@@ -8,3 +8,35 @@ require 'input_module.rb'
 # call prompt methods to handle user inputs
 url = Input.prompt_shop_url
 products_to_find = Input.prompt_products_list
+
+
+# initialize product count and price hashes
+Shop.create_count_hash(products_to_find)
+Shop.create_price_hash(products_to_find)
+
+
+# process products json on each page until reached end of page
+end_of_page = false
+page = 1
+until end_of_page
+    
+    base_url = url + page.to_s
+    #format :json
+    
+    # connect to current base url
+    res = Url.read_response(base_url)
+    
+    # parse the response into json
+    json = Url.parse_response(res)
+    
+    # extract array from json hash
+    products_array = json["products"]
+    
+    # check if reached end of json result
+    end_of_page = Shop.is_page_end(products_array)
+    
+    # iterate over json to count products
+    Shop.count(products_array)
+    
+    page += 1
+end
